e17aba95fcd9c83aec5cfcf493726f75679ab37a
cyrilchukwuebuka <muofunanya3@gmail.com>
2021-11-03 19:07:25 +0100

    initial commit, also cleaned project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="app">
+      <h1> COVID-19 TRACKER</h1>
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default App;
