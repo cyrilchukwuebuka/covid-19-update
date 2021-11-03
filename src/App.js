af56673061e519932eb9ddba1ec250491624f949
cyrilchukwuebuka <muofunanya3@gmail.com>
2021-11-03 22:55:15 +0100

    Worked on the Dropdown data fetched from API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import "./App.css";
 import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useState, useEffect } from "react";
 
 function App() {
+  //  https://disease.sh​/v3​/covid-19​/countries
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(["Worldwide"])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      try{
+        await fetch("https://disease.sh/v3/covid-19/countries?sort=cases&allowNull=allowNull")
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          const countries = data.map(country => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso3
+            }
+          ));
+          setCountries(countries)
+        })
+      } catch(err){
+        console.log(err)
+      }
+    }
+
+    getCountriesData()
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode)
+  }
+
   return (
     <div className="app">
-      <h1> COVID-19 TRACKER</h1>
-      <FormControl className="app_dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app_header">
+        <h1> COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+          <MenuItem value="Worldwide">Worldwide</MenuItem>
+          {countries.map(country => (
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
