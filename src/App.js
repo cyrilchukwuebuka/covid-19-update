ba9a3ec0329dcb3483520b21581dbc1bf7bc485e
cyrilchukwuebuka <muofunanya3@gmail.com>
2021-11-08 09:50:09 +0100

    worked on the Map components and workflow

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import Map from "./components/Map/Map";
 import Table from "./components/Table/Table"
 import {sortData} from "./utils/utils"
 import LineGraph from "./components/LineGraph/LineGraph";
-import 'leaflet/dist/leaflet.css'
+// The Leaflet Css is added via HTML link, though below still works same
+// import 'leaflet/dist/leaflet.css'
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState(["Worldwide"]);
   const [countryInfo, setCountryInfo] = useState({});
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState([51.09, -0.09])
+  const [mapZoom, setMapZoom] = useState(3)
+  const [mapCountries, setMapCountries] = useState([])
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all?allowNull=allowNull')
@@ -36,6 +40,7 @@ function App() {
             ));
             const sortedData = sortData(data)
             setTableData(sortedData)
+            setMapCountries(data)
             setCountries(countries)
           })
       } catch (err) {
@@ -56,8 +61,12 @@ function App() {
     await fetch(url)
     .then(response => response.json())
     .then(data => {
+      console.log(data)
       setCountry(countryCode)
       setCountryInfo(data)
+
+      countryCode !== 'Worldwide' ? setMapCenter([data.countryInfo.lat, data.countryInfo.long]) : setMapCenter([-51.09, 0.09])
+      setMapZoom(4)
     })
   }
 
@@ -66,11 +75,11 @@ function App() {
       <div className="app_left">
         <div className="app_header">
           <h1> COVID-19 TRACKER</h1>
-          <FormControl className="app_dropdown">
+          <FormControl className="app_dropdown" style={{ color: 'white', backgroundColor: 'white' }}>
             <Select variant="outlined" value={country} onChange={onCountryChange}>
-              <MenuItem value="Worldwide">Worldwide</MenuItem>
+              <MenuItem value="Worldwide" style={{ color: 'white', backgroundColor: 'grey' }}>Worldwide</MenuItem>
               {countries.map(country => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
+                <MenuItem value={country.value} style={{color: 'white', backgroundColor: 'grey'}}>{country.name}</MenuItem>
               ))}
             </Select>
           </FormControl>
@@ -81,10 +90,10 @@ function App() {
           <InfoBox title='Recovered' total={countryInfo.recovered} cases={countryInfo.todayRecovered} />
           <InfoBox title='Deaths' total={countryInfo.deaths} cases={countryInfo.todayDeaths} />
         </div>
-        <Map />
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom}/>
       </div>
 
-      <Card className="app_right">
+      <Card className="app_right" >
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Map.css'
-// import {Map as LeafletMap, TileLayer} from 'react-leaflet'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { showDataOnMap } from '../../utils/utils'
 
-function Map() {
+function Map({ countries, caseType, center, zoom }) {
+    // console.log("========", center)
+    // const [mapCenter, setMapCenter] = useState([50.91, 0.09])
+
+    // useEffect(() => {
+    //     setMapCenter(center)
+    // }, [center])
+ 
     return (
-        <div className='map'>
-            
-        </div>
+        <MapContainer className="map" center={center} zoom={zoom} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {showDataOnMap(countries, caseType)}
+        </MapContainer>
+
     )
 }
 
 export default Map
+
